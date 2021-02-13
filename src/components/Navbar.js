dcd974c53535810b1410b94e0d8e3755ae89d583
Stefaan <safwebdev@gmail.com>
2021-02-14 00:34:16 +0800

    create pages and enable routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/">MiCasa</NavLink>
+          <NavLink to="/post">Blog Posts</NavLink>
+          <NavLink to="/project">Projects</NavLink>
+          <NavLink to="/about">About Me</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
